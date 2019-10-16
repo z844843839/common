1ad01800f718a9489cbfe8a40b8234bc85b8c7e9
王龙龙 <wanglwanglonglong@e6yun.com>
2019-10-16 15:30:14 +0800

    添加RPC请求时携带header参数配置

---
+++ common-base/src/main/java/com/crt/common/config/FeginClientConfig.java
@@ -0,0 +1,30 @@
+package com.crt.common.config;
+
+import feign.RequestInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+
+@Component
+public class FeginClientConfig {
+
+    @Bean
+    public RequestInterceptor headerInterceptor() {
+        return template -> {
+            ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+            HttpServletRequest request = attributes.getRequest();
+            Enumeration<String> headerNames = request.getHeaderNames();
+            if (headerNames != null) {
+                while (headerNames.hasMoreElements()) {
+                    String name = headerNames.nextElement();
+                    String values = request.getHeader(name);
+                    template.header(name, values);
+                }
+            }
+        };
+    }
+}
