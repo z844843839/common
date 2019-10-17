1b5a03c11eb7433b1837228dd4fd61bf7e79a448
王龙龙 <wanglwanglonglong@e6yun.com>
2019-10-17 16:34:04 +0800

    营业执照 统一社会信用代码校验

--- common-base/src/main/java/com/crt/common/util/VerifyUtil.java
+++ common-base/src/main/java/com/crt/common/util/VerifyUtil.java
@@ -1,5 +1,7 @@
 package com.crt.common.util;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.util.Random;
@@ -82,5 +84,43 @@ public class VerifyUtil {
         Color color = new Color(ran.nextInt(256),
                 ran.nextInt(256), ran.nextInt(256));
         return color;
+    }
+
+    /**
+     * 营业执照 统一社会信用代码（18位）
+     * @param license
+     * @return
+     */
+    public static boolean isLicense18(String license) {
+        if(StringUtils.isEmpty(license)) {
+            return false;
+        }
+        if(license.length() != 18) {
+            return false;
+        }
+
+        String regex = "^([159Y]{1})([1239]{1})([0-9ABCDEFGHJKLMNPQRTUWXY]{6})([0-9ABCDEFGHJKLMNPQRTUWXY]{9})([0-90-9ABCDEFGHJKLMNPQRTUWXY])$";
+        if (!license.matches(regex)) {
+            return false;
+        }
+        String str = "0123456789ABCDEFGHJKLMNPQRTUWXY";
+        int[] ws = { 1, 3, 9, 27, 19, 26, 16, 17, 20, 29, 25, 13, 8, 24, 10, 30, 28 };
+        String[] codes = new String[2];
+        codes[0] = license.substring(0, license.length() - 1);
+        codes[1] = license.substring(license.length() - 1, license.length());
+        int sum = 0;
+        for (int i = 0; i < 17; i++) {
+            sum += str.indexOf(codes[0].charAt(i)) * ws[i];
+        }
+        int c18 = 31 - (sum % 31);
+        if (c18 == 31) {
+            c18 = 'Y';
+        } else if (c18 == 30) {
+            c18 = '0';
+        }
+        if (str.charAt(c18) != codes[1].charAt(0)) {
+            return false;
+        }
+        return true;
     }
 }
