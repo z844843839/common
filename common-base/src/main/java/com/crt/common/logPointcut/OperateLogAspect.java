c0ea059033767294ec4c3cb62a11b09a6a897618
王龙龙 <wanglwanglonglong@e6yun.com>
2019-11-07 21:01:58 +0800

    对日志进行try

--- common-base/src/main/java/com/crt/common/logPointcut/OperateLogAspect.java
+++ common-base/src/main/java/com/crt/common/logPointcut/OperateLogAspect.java
@@ -65,74 +65,89 @@ public class OperateLogAspect {
         String msg;
         try {
             log.error("<============ 进入环绕通知 ============>");
-            Object target = point.getTarget();
-            Object[] args = point.getArgs();
-            Object param = null;
-            if (args != null && args.length > 0){
-                for (int k = 0; k < args.length; k++) {
-                    Object arg = args[k];
-                    // 获取对象类型
-                    String typeName = arg.getClass().getTypeName();
-                    for (String t : Constants.BASIC_TYPES) {
-                        //1 判断是否是基础类型
-                        if (!t.equals(typeName)) {
-                            //Controller 入参的PO
-                            param = arg;
+            Object target = null;
+            try {
+                target = point.getTarget();
+                Object[] args = point.getArgs();
+                Object param = null;
+                if (args != null && args.length > 0){
+                    for (int k = 0; k < args.length; k++) {
+                        Object arg = args[k];
+                        // 获取对象类型
+                        String typeName = arg.getClass().getTypeName();
+                        for (String t : Constants.BASIC_TYPES) {
+                            //1 判断是否是基础类型
+                            if (!t.equals(typeName)) {
+                                //Controller 入参的PO
+                                param = arg;
+                            }
                         }
                     }
                 }
-            }
-            //操作类型 默认为空
-            logJson.put("operateType","");
+                //操作类型 默认为空
+                logJson.put("operateType","");
+            } catch (Exception e) {}
             //执行原方法
             Object operateResult = point.proceed();
-            //操作模块
-            String module = request.getRequestURI();
-            module = module.substring(1,module.length());
-            module = module.substring(0,module.indexOf("/"));
-            logJson.put("operateModule",module);
-            //操作方法
-            String operateMethod = target.getClass().getName() +"."+ point.getSignature().getName() +"()";
-            logJson.put("operateMethod",operateMethod);
-            //操作请求
-            logJson.put("operateUrl",request.getRequestURL().toString());
-            //操作内容
-            Signature signature = point.getSignature();
-            MethodSignature methodSignature = (MethodSignature)signature;
-            //获取方法对象
-            Method targetMethod = methodSignature.getMethod();
-            //获取方法上注解
-            ApiOperation api = targetMethod.getAnnotation(ApiOperation.class);
-            logJson.put("operateContent",api.value());
-            //获取方法上注解
-            TokenAuthentication auth = targetMethod.getAnnotation(TokenAuthentication.class);
-            if (null == auth || !AuthLevel.NO_AUTH.equals(auth.authLevel())){
-                //当前操作用户
-                logJson.put("operaterCode",UserInfoUtil.getLoginUserCode());
-                logJson.put("operaterName",UserInfoUtil.getLoginUserRealName());
-            }
-            logJson.put("createdAt",new Date());
-            Integer operaterResult = Constants.RESULT_UNKNOWN;
-            if (null != operateResult){
-                E6Wrapper e6 = (E6Wrapper)operateResult;
-                if (Constants.STATUS_CODE_SUCCESS.equals(e6.getCode())){
-                    operaterResult = Constants.RESULT_SUCCESS;
-                }else {
-                    operaterResult = Constants.RESULT_FAIL;
-                }
-                logJson.put("result",operaterResult);
-            }
             try {
-                executorService.execute(new MqRunnable(messageQueueService,logJson.toJSONString()));
-            }catch (Exception e){
-                log.error("MQ调用异常{}", e.getMessage());
-            }
+                //操作模块
+                String module = request.getRequestURI();
+                module = module.substring(1,module.length());
+                module = module.substring(0,module.indexOf("/"));
+                logJson.put("operateModule",module);
+                //操作方法
+                String operateMethod = target.getClass().getName() +"."+ point.getSignature().getName() +"()";
+                logJson.put("operateMethod",operateMethod);
+                //操作请求
+                logJson.put("operateUrl",request.getRequestURL().toString());
+                //操作内容
+                Signature signature = point.getSignature();
+                MethodSignature methodSignature = (MethodSignature)signature;
+                //获取方法对象
+                Method targetMethod = methodSignature.getMethod();
+                //获取方法上注解
+                ApiOperation api = targetMethod.getAnnotation(ApiOperation.class);
+                logJson.put("operateContent",api.value());
+                //获取方法上注解
+                TokenAuthentication auth = targetMethod.getAnnotation(TokenAuthentication.class);
+                if (null == auth || !AuthLevel.NO_AUTH.equals(auth.authLevel())){
+                    //当前操作用户
+                    logJson.put("operaterCode",UserInfoUtil.getLoginUserCode());
+                    logJson.put("operaterName",UserInfoUtil.getLoginUserRealName());
+                }
+                logJson.put("createdAt",new Date());
+                Integer operaterResult = Constants.RESULT_UNKNOWN;
+                if (null != operateResult){
+                    E6Wrapper e6 = (E6Wrapper)operateResult;
+                    if (Constants.STATUS_CODE_SUCCESS.equals(e6.getCode())){
+                        operaterResult = Constants.RESULT_SUCCESS;
+                    }else {
+                        operaterResult = Constants.RESULT_FAIL;
+                    }
+                    logJson.put("result",operaterResult);
+                }
+                try {
+                    executorService.execute(new MqRunnable(messageQueueService,logJson.toJSONString()));
+                }catch (Exception e){
+                    log.error("MQ调用异常{}", e.getMessage());
+                }
+            } catch (Exception e) {}
             return operateResult;
         } catch (Throwable throwable) {
-            throwable.getMessage();
+            String ex_msg= stackTraceToString(throwable.getClass().getName(), throwable.getMessage(), throwable.getStackTrace());
+            log.error("异常信息:"+ex_msg);
             msg = "系统繁忙，请稍后重试";
             return E6WrapperUtil.ok(Constants.INTERNAL_SERVER_ERROR,msg,null);
         }
+    }
+
+    public String stackTraceToString(String exceptionName, String exceptionMessage, StackTraceElement[] elements) {
+        StringBuffer strbuff = new StringBuffer();
+        for (StackTraceElement stet : elements) {
+            strbuff.append(stet + "\n");
+        }
+        String message = exceptionName + ":" + exceptionMessage + "\n\t" + strbuff.toString();
+        return message;
     }
 
     class MqRunnable implements Runnable {
