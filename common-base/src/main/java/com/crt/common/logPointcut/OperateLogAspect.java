d0fc0d1eb34139a0884f2c5cea0083e0d477bc39
王龙龙 <wanglwanglonglong@e6yun.com>
2019-11-08 18:20:22 +0800

    修改操作日志记录

--- common-base/src/main/java/com/crt/common/logPointcut/OperateLogAspect.java
+++ common-base/src/main/java/com/crt/common/logPointcut/OperateLogAspect.java
@@ -26,6 +26,8 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import java.lang.reflect.Method;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Date;
 import java.util.concurrent.ExecutorService;
 
@@ -94,12 +96,15 @@ public class OperateLogAspect {
                 String module = request.getRequestURI();
                 module = module.substring(1,module.length());
                 module = module.substring(0,module.indexOf("/"));
+                if("MIDDLE-LOG-OPERATE".equals(module)){//日志模块不记录
+                    return operateResult;
+                }
                 logJson.put("operateModule",module);
                 //操作方法
                 String operateMethod = target.getClass().getName() +"."+ point.getSignature().getName() +"()";
                 logJson.put("operateMethod",operateMethod);
                 //操作人IP
-                logJson.put("operateIp",request.getRemoteAddr());
+                logJson.put("operateIp",getIpAddress(request));
                 //操作请求
                 logJson.put("operateUrl",request.getRequestURL().toString());
                 //操作内容
@@ -143,6 +148,43 @@ public class OperateLogAspect {
             msg = "系统繁忙，请稍后重试";
             return E6WrapperUtil.ok(Constants.INTERNAL_SERVER_ERROR,msg,null);
         }
+    }
+
+    /**
+     * 获取用户真实IP地址，不使用request.getRemoteAddr();的原因是有可能用户使用了代理软件方式避免真实IP地址。
+     * 可是，如果通过了多级反向代理的话，X-Forwarded-For的值并不止一个，而是一串IP值，究竟哪个才是真正的用户端的真实IP呢？
+     * 答案是取X-Forwarded-For中第一个非unknown的有效IP字符串
+     * @param request
+     * @return
+     */
+    private static String getIpAddress(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+            if("127.0.0.1".equals(ip)||"0:0:0:0:0:0:0:1".equals(ip)){
+                //根据网卡取本机配置的IP
+                InetAddress inet=null;
+                try {
+                    inet = InetAddress.getLocalHost();
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                }
+                ip= inet.getHostAddress();
+            }
+        }
+        return ip;
     }
 
     public String stackTraceToString(String exceptionName, String exceptionMessage, StackTraceElement[] elements) {
